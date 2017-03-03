abed943095679aa633963378853e15ef3929e431
excellent-1 (SourceTree) <ExcellentInDevelopment@gmail.com>
2017-03-03 17:29:21 -0500

    Initial Check-In for MVC5 KnockoutJS ReactJS Application

---
+++ mvc5/Startup.cs
@@ -0,0 +1,16 @@
+﻿using mvc5;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
